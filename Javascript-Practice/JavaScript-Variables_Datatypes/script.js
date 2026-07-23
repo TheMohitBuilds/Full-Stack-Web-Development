9216e36aaa7952a1f66f4af9a7060cddd37e0557
TheMohitBuilds <mohit120577@gmail.com>
2026-07-23 21:37:37 +0530

    Added JavaScript-Variables practice files in Javascript - variables and datatypes folder

---
+++ Javascript-Practice/JavaScript-Variables_Datatypes/script.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 7;
+
+a = a + 1
+
+console.log(a + b )
+console.log(a)
+console.log(typeof a, typeof b)
+
+{
+    let a = 125;
+    console.log(a)
+}
+
+const a1 = 5;
+//a1 = a1 + 1 (This summation is not allowed in it because a1 is constant)
+console.log(a1)
