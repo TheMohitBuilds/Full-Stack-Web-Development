62a9ac765493f2bf81e07e82e2aaf058388e01fb
TheMohitBuilds <mohit120577@gmail.com>
2026-07-23 22:09:40 +0530

    Added datatypes&Objects Js practice file in JavaScript-Variables_datatypes folder

---
+++ Javascript-Practice/JavaScript-Variables_Datatypes/datatypes&Object.js
@@ -0,0 +1,20 @@
+//Primitive datatype
+let x = "TheMohitBuilds";
+let y = 125;
+let z = 7.77;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+//Object
+let o = {
+    Name: "Mohit",
+    Age: "18",
+}
+console.log(o);
+
+o.NetWorth = "Zero crores"
+console.log(o);
