b958905776199f7d5b75d31199670022c259f314
TheMohitBuilds <mohit120577@gmail.com>
2026-07-23 15:14:04 +0530

    Added Javascript-introduction practice folder and two script.js file along with it

---
+++ Javascript-Practice/JavaScript-Introduction/script.js
@@ -0,0 +1,10 @@
+alert("Hello World")
+
+console.log("Future Full-Stack-AI-Engineer")
+console.log("HTML | CSS | JavaScript | Python | C++")
+console.log("TheMohitBuilds")
+
+var a = prompt("Enter the Number: ")
+console.log("The number is " +a)
+
+document.body.style.backgroundColor = "aquamarine"
---
+++ Javascript-Practice/JavaScript-Introduction/scriptNew.js
@@ -0,0 +1,3 @@
+console.log("Hello World")
+console.log("TheMohitBuilds")
+console.log("Full-Stack-AI-Engineer in the making")
